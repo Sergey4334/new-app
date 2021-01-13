b9cb53ac0083e6e91df0b590cd4dc49811b06fd3
Sergey4334 <sergey.melnichuk92@gmail.com>
2021-01-13 11:55:44 +0300

    Added funtinal news application

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //* Hi! New Project
-const apiUrl = 'https://news-api-v2.herokuapp.com';
+//const apiUrl = 'https://news-api-v2.herokuapp.com';
 
 // Custom Http Module
 function customHttp() {
@@ -59,7 +59,66 @@ function customHttp() {
 // Init http module
 const http = customHttp();
 
+const newsService = (function () {
+  const apiKey = 'd1140151092a426bb672b40dedba49cd';
+  const apiUrl = 'https://news-api-v2.herokuapp.com';
+
+  return {
+    tophedlines(country = 'ua', cb) {
+      http.get(`${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`,cb);
+     },
+    everything(query, cb) {
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`,cb);
+     },
+  };
+}());
+
 //  init selects
 document.addEventListener('DOMContentLoaded', function() {
   M.AutoInit();
+  loadNews();
 });
+
+//* Load News
+
+function loadNews() {
+  newsService.tophedlines('ua', onGetResponse);
+}
+
+//* Function on get rsponse from Server
+
+function onGetResponse(err, res) {
+  renderNews(res.articles);
+}
+//* Function render news
+
+function renderNews(news) {
+  const newsContainer = document.querySelector('.news-container .row');
+  let fragment = '';
+  news.forEach(newsItem => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+  newsContainer.insertAdjacentHTML('afterbegin', fragment);
+}
+//*News Item template function
+
+function newsTemplate({urlToImage, title, url, description}) {
+  //console.log(urlToImage);
+  return `
+    <div class="col s12">
+      <div class="card">
+        <div class="card-image">
+          <img src="${urlToImage}"/>
+          <span class="card-title">${title || ''}</span>
+        </div>
+        <div class="card-content">
+          <p>${description || ''}</p>
+        </div>
+        <div class="card-action">
+          <a href="${url}" target="_blank">Read more...</a>
+        </div>
+      </div>
+    </div>
+  `;
+}
